1a426c82e3aec447c581577d300472eeb1b0ebcb
Maksym Pakula <gargantadoto@gmail.com>
2026-04-09 03:07:34 +0300

    Add project settings

---
+++ settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Cities Temperature Management API"
+
+    DATABASE_URL: str | None = "sqlite:///./city-temperature.db"
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+settings = Settings()
