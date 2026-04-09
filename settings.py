f51b0da651f82a9bb060ff9d5a7fc366bde1f8ef
Maksym Pakula <gargantadoto@gmail.com>
2026-04-09 05:35:16 +0300

    Add fetch temperature function and hide API_KEY to .env variables

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Cities Temperature Management API"
-
     DATABASE_URL: str | None = "sqlite:///./city-temperature.db"
+    API_KEY: str | None = None
 
     class Config:
         case_sensitive = True
---
+++ temperature/crud.py
@@ -0,0 +1,26 @@
+import os
+
+import httpx
+from dotenv import load_dotenv
+from sqlalchemy.orm import Session
+
+from temperature import models
+
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+URL = "https://api.weatherapi.com/v1/current.json"
+
+
+async def fetch_temperature(city_name: str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            URL,
+            params={
+                "key": API_KEY,
+                "q": city_name
+            }
+        )
+        response.raise_for_status()
+        return response.json()
